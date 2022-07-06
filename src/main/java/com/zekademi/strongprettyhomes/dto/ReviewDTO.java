868e73d17b7b78a0ba410b07a0496317d9b2a6a8
Zekeriya CANBAL <zekeriyacanbal@gmail.com>
2022-07-06 10:46:50 +0300

    Update ReviewDTO.java

--- src/main/java/com/zekademi/strongprettyhomes/dto/ReviewDTO.java
+++ src/main/java/com/zekademi/strongprettyhomes/dto/ReviewDTO.java
@@ -24,6 +24,7 @@ public class ReviewDTO {
     private Integer score;
     private Long propertyId;
     private Long userId;
+    private PropertyCategory category;
     private ReviewStatus status;
 
 
