548ae487c9887a32ae7236c00ade1b0f5dfa2f91
Zekeriya CANBAL <zekeriyacanbal@gmail.com>
2022-07-08 10:18:53 +0300

    Update PropertyController.java

--- src/main/java/com/zekademi/strongprettyhomes/controller/PropertyController.java
+++ src/main/java/com/zekademi/strongprettyhomes/controller/PropertyController.java
@@ -109,7 +109,7 @@ public class PropertyController {
     @PostMapping("/{id}/count")
     @PreAuthorize("hasRole('CUSTOMER')")
     public ResponseEntity<Map<String, Boolean>> postNewLikeCount(@PathVariable Long id, 
-                                                                 @RequestParam("likeCount") Long likeCount)
+                                                                 @RequestParam("likeCount") Long likeCount
 ){
 
         propertyService.newLikeCount(id,likeCount);
