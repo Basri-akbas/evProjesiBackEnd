d62e909d0f523ff6a5c0ef8dd7ea4624490765ba
Zekeriya CANBAL <zekeriyacanbal@gmail.com>
2022-07-25 10:27:31 +0200

    Update Review.java

--- src/main/java/com/zekademi/strongprettyhomes/domain/Review.java
+++ src/main/java/com/zekademi/strongprettyhomes/domain/Review.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import org.hibernate.annotations.Type;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
